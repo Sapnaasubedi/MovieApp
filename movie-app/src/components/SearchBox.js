3b9381f50d89cd8363832c111d6bc30c8b549f75
Sapana Subedi <sapanasubedi@takeo.ai>
2024-08-15 11:09:29 +0545

    feat: change color

--- movie-app/src/components/SearchBox.js
+++ movie-app/src/components/SearchBox.js
@@ -6,7 +6,7 @@ const SearchBox = () => {
     const onSearch = (value, _e, info) => console.log(info?.source, value);
 
   return (
-    <div style={{ margin: 30 }}> <Input size="large" onSearch={onSearch} placeholder="search" suffix={<SearchOutlined />} style={{background:'#094887' , border:'2px solid #05294d', borderRadius:'8'}}/>
+    <div style={{ margin: 30 }}> <Input size="large" onSearch={onSearch} placeholder="search" suffix={<SearchOutlined />} style={{background:'#094887' , border:'2px solid #0B0770', borderRadius:'8'}}/>
 </div>
   )
 }
