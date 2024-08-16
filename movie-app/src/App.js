86e5734e5032f02c9dbcb1fd014e7e33f6867da0
Sapana Subedi <sapanasubedi@takeo.ai>
2024-08-16 10:50:30 +0545

    feat: style card component

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -5,15 +5,14 @@ import MovieCard from "./components/MovieCard";
 
 const App = () => {
   return (
-   
-      <Row style={{ backgroundColor: "#094887" }} >
-       
-        <Col span={4} offset={18}>
-          <SearchBox />
-          <MovieCard/>
-        </Col>
-      </Row>
-    
+    <Row style={{ backgroundColor: "#094887" }}>
+      <Col span={4} offset={18}>
+        <SearchBox />
+      </Col>
+      <Col span={24} offset={2}>
+        <MovieCard />
+      </Col>
+    </Row>
   );
 };
 
--- movie-app/src/components/MovieCard.js
+++ movie-app/src/components/MovieCard.js
@@ -1,9 +1,20 @@
-import React from 'react'
-
+import React from "react";
+import { Card } from "antd";
 const MovieCard = () => {
   return (
-    <div>MovieCard</div>
-  )
-}
+    <div>
+      <Card
+        hoverable
+        style={{ width: 240 }}
+        cover={
+          <img
+            alt="example"
+            src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+          />
+        }
+      ></Card>
+    </div>
+  );
+};
 
-export default MovieCard
+export default MovieCard;
